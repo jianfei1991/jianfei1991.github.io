4c1bc90426256e77900a7f4680d26d85ea5a328b
jianfei1991 <wujianfei1991@gmail.comm>
2015-08-31 17:09:32 +0800

    change nav

---
+++ js/common.js
@@ -0,0 +1,10 @@
+//nav
+
+{
+	"主页":"index.html",
+	"jsonp":"jsonp.html",
+	"安卓测试":"meta_android.html",
+	"viewport":"viewport.html",
+	"css3时钟":"cssClock2.html",
+	"手机js兼容测试":"jianrongTestInMoible.html"
+}
